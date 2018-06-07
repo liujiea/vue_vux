8ceb37c665db738f04bbfbe39c4d0a695400d176
liujie <751310422@qq.com>
2018-06-07 11:45:23 +0800

    frist commit

---
+++ src/components/icon.vue/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <i :class="[className, isMsg ? 'weui-icon-msg' : '']"></i>
+</template>
+<script>
+    export default {
+        name: 'icon',
+        props: {
+            type: String,
+            isMsg: Boolean
+        },
+        computed: {
+            className() {
+                return `weui-icon weui_icon_${this.type} weui-icon-${this.type.replace(/ _/g, '-')}`
+            }
+        }
+    }
+</script>
+<style lang="less">
+    @import '../../style/weui/icon/weui_icon_font';
+
+    .icon-big:before {
+        font-size: 30px;
+    }
+</style>
