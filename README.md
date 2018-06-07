cd1237d1b08b8dc883d99e3a13dab211cae3cddf
liujie <751310422@qq.com>
2018-06-07 21:23:50 +0800

    REAMDE

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 > A Vue.js project
 
+> 2018年6月7日21:17:31 提交
+    - 构建完成第一个组件icon
+    - 通用less
+        - mobile, setArrow, setOnepx, text
+        - color, global, weui-button, weui-cell, weui-dialog, weui-grid, weui-msg, weui-progress
+        - fn
+        - icon -> weui_iocn, weui_icon_font
+        - variable(全局)
+
 ## Build Setup
 
 ``` bash
--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <v-icon type="success"></v-icon>
+    <v-icon type="info"></v-icon>
+    <v-icon type="info-circle"></v-icon>
+    <v-icon type="warn"></v-icon>
+    <v-icon type="waiting"></v-icon>
+    <v-icon type="waiting-circle"></v-icon>
+    <v-icon type="safe-success"></v-icon>
+    <v-icon type="safe-warn"></v-icon>
+    <v-icon type="success-circle"></v-icon>
+    <v-icon type="success-no-circle"></v-icon>
+    <v-icon type="circle"></v-icon>
+    <v-icon type="download"></v-icon>
+    <v-icon type="cancel"></v-icon>
+    <v-icon type="search"></v-icon>
+    <v-icon type="clear"></v-icon>
+    <v-icon type="success" is-msg></v-icon>
+    <v-icon type="info" is-msg></v-icon>
+    <v-icon type="safe-success" is-msg></v-icon>
+    <v-icon type="safe-warn" is-msg></v-icon>
   </div>
 </template>
 
 <script>
+    import VIcon from './components/icon/icon'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    VIcon
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
